67170241087e2cc513cc73bdb5820ba8cdddb97e
Nehan21-Gaidhani <nehanshugaidhani@gmail.com>
2025-05-15 18:44:45 +0000

    user changes

--- models/User.js
+++ models/User.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  passwordHash: { type: String },
-  verified: { type: Boolean, default: false },
-  verificationToken: { type: String },
-});
+
 
 module.exports = mongoose.model('User', userSchema);
