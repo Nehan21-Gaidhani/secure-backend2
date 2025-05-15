4fb74260f60a816df01b84a5e86e80dacffd2182
Nehan21-Gaidhani <nehanshugaidhani@gmail.com>
2025-05-15 18:49:56 +0000

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -69,8 +69,7 @@ router.post('/register', async (req, res) => {
 });
 
 // ✅ GET /verify-registration 
-router.get('/verify-registration', async (req, res) => {
-  const { token } = req.query;
+
 
   try {
     const { email } = jwt.verify(token, process.env.JWT_SECRET);
