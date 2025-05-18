9245d126e151990598342892fdd3668c5bebe1f2
Nehan21-Gaidhani <nehanshugaidhani@gmail.com>
2025-05-18 10:02:57 +0000

    commit

---
+++ test-cli.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const email = "nehanshugaidhani@gmail.com";
+const password = "tjgbeawjqfxqoljk";
+const TOTAL_ATTEMPTS = 10;
+
+(async () => {
+  for (let i = 0; i < TOTAL_ATTEMPTS; i++) {
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password
+      });
+      console.log(`[${i + 1}] ✅ Success:`, res.data);
+    } catch (err) {
+      console.log(`[${i + 1}] ❌ Error:`, err.response?.data || err.message);
+    }
+  }
+})();
